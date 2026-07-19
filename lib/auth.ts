e04cb46c4e31a2634ad9de96c55f4c14469683f9
tonymac129 <tonyhsu129@gmail.com>
2026-07-19 00:04:47 -0400

    fix: discord and github oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,5 +10,13 @@ export const auth = betterAuth({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
+    discord: {
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }, //TODO: add facebook oauth
   },
 });
